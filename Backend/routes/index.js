733ffafaace7e3f8c0558aa87f51a0ca1668e845
PedroMayoral10 <pedromayoralinsti@gmail.com>
2025-11-19 12:00:40 +0100

    Borrado de archivos innecesarios

--- Backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({mensaje: 'Hola desde el servidor'});
-});
-
-module.exports = router;
